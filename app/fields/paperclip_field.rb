2288458501b654b156a7947de872274b582e4117
devappro <dev.webappro@gmail.com>
2015-12-24 19:16:37 +0000

    Ajout partials & helpers

---
+++ app/fields/paperclip_field.rb
@@ -0,0 +1,31 @@
+require "administrate/fields/base"
+
+class PaperclipField < Administrate::Field::Base
+  
+  
+  def url
+    data.url
+  end
+
+
+
+
+  def thumbnail
+    data.url(:thumb)
+  end
+  
+  
+  
+  
+  def medium
+    data.url(:medium)
+  end
+  
+  
+  
+
+  def to_s
+    data
+  end
+  
+end
